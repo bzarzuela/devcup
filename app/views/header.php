6608753e881972267669b52d6b053e0546644f18
Nicole <nasdelrosario@gmail.com>
2014-08-16 14:06:56 +0800

    UI improvements
    All browsable pages are now up

---
+++ app/views/header.php
@@ -0,0 +1,32 @@
+<!-- Navigation -->
+<nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+                <i class="fa fa-bars"></i>
+            </button>
+            <a class="navbar-brand" href="/">NOUS</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+            <ul class="nav navbar-nav">
+                <li>
+                    <a href="<?php echo url('about') ?>">About</a>
+                </li>
+                <li>
+                    <a href="<?php echo url('connect') ?>">Connect</a>
+                </li>
+                <li>
+                    <a href="<?php echo url('pricing') ?>">Pricing</a>
+                </li>                
+                <li>
+                    <a href="<?php echo url('contact') ?>">Contact</a>
+                </li>
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container -->
+</nav>
