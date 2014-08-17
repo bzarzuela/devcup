ed25ebe28fde1209d692b2a4e8970a196dad7b83
Nicole <nasdelrosario@gmail.com>
2014-08-17 12:39:11 +0800

    Configured

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('lucid32'),
+	'local' => array('lucid32','Airgore.local','airgore'),
 
 ));
 
